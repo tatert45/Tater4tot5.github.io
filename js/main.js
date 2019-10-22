da2060b5ae0438c9d7a8a0ee8e02dcfa63a11ec6
Tate Janek <tatejanek@gmail.com>
2019-10-22 15:22:29 -0500

    updates to otherwork

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ $(function() {
    }
  });
 });
+$(document).ready(function(){
+				$(".filter-button").click(function(){
+					var value = $(this).attr('data-filter');
+					if(value == "all")
+					{
+						$('.filter').show();
+					}
+					else
+					{
+						$(".filter").not('.'+value).hide();
+						$('.filter').filter('.'+value).show();
+					}
+					if ($(".filter-button").removeClass("active")) {
+						$(this).addClass("active");
+					}
+				});
+				$(this).addClass("active");
+			});
